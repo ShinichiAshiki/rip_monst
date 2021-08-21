9b2d6f0fd79ae7381d754086a4d949a3d7353274
ShinichiAshiki <71955393+ShinichiAshiki@users.noreply.github.com>
2021-08-21 21:23:56 +0900

    Update forcast.py

--- forcast.py
+++ forcast.py
@@ -1,4 +1,3 @@
-import datetime, time
 import requests, sys
 import bs4
 import IPython
@@ -21,34 +20,32 @@ dicChanceKinds = {
 mntrList = [
     "超絶大チャンス"
 ]
-while True:
-    ##monitor
-    IPython.display.clear_output()
-    try:
-        res = requests.get(gachaforcastURL)
-        res.raise_for_status()
-        soupTgtForecast = bs4.BeautifulSoup(res.text, "html.parser")
-        elmImg = soupTgtForecast.select_one('img.img-responsive.animated')
-        elmRates = soupTgtForecast.select('tr')
-        if (elmImg == None) or (elmRates == None):
-            messages = linebot.models.TextSendMessage(text = "サイトの仕様が変わった可能性があります.")
-            linebot.LineBotApi(CAT).push_message(myID, messages = messages)
-        else:
-            chance = ""
-            for dickey in dicChanceKinds.keys():
-                if dickey in elmImg.attrs['src']:
-                    chance = dicChanceKinds[dickey]
-                    break
-            if chance in mntrList:
-                if float(elmRates[2].text.replace("\n","").replace("5分間の★5確率","").replace("％",""))>=30.0:
-                    lineMsg = chance + "です\n"
-                    lineMsg += elmRates[2].text.replace('\n','')
-                    messages = linebot.models.TextSendMessage(text = lineMsg)
-                    linebot.LineBotApi(CAT).push_message(CHANNEL_ID, messages = messages)
-            else:
-                print( chance + "でした" )
-    except:
-        print(sys.exc_info())
-        messages = linebot.models.TextSendMessage(text = "ミスりました.")
+##monitor
+IPython.display.clear_output()
+try:
+    res = requests.get(gachaforcastURL)
+    res.raise_for_status()
+    soupTgtForecast = bs4.BeautifulSoup(res.text, "html.parser")
+    elmImg = soupTgtForecast.select_one('img.img-responsive.animated')
+    elmRates = soupTgtForecast.select('tr')
+    if (elmImg == None) or (elmRates == None):
+        messages = linebot.models.TextSendMessage(text = "サイトの仕様が変わった可能性があります.")
         linebot.LineBotApi(CAT).push_message(myID, messages = messages)
-    time.sleep(30)
+    else:
+        chance = ""
+        for dickey in dicChanceKinds.keys():
+            if dickey in elmImg.attrs['src']:
+                chance = dicChanceKinds[dickey]
+                break
+        if chance in mntrList:
+            if float(elmRates[2].text.replace("\n","").replace("5分間の★5確率","").replace("％",""))>=30.0:
+                lineMsg = chance + "です\n"
+                lineMsg += elmRates[2].text.replace('\n','')
+                messages = linebot.models.TextSendMessage(text = lineMsg)
+                linebot.LineBotApi(CAT).push_message(CHANNEL_ID, messages = messages)
+        else:
+            print( chance + "でした" )
+except:
+    print(sys.exc_info())
+    messages = linebot.models.TextSendMessage(text = "ミスりました.")
+    linebot.LineBotApi(CAT).push_message(myID, messages = messages)
